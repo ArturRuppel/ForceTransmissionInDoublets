800ea8ec9fe9d3b92348bdf2bfa00c09203788b0
Artur Ruppel <87201931+MRT9393@users.noreply.github.com>
2022-10-26 16:50:31 +0200

    Update Fig6.py

--- Fig6.py
+++ Fig6.py
@@ -14,13 +14,19 @@ from scipy.stats import zscore
 from plot_and_filter_functions import *
 from scipy.interpolate import interp1d
 
+# define some colors for the plots
+colors_parent = ['#026473', '#E3CC69', '#77C8A6', '#D96248']
+colors_parent_dark = ['#01353D', '#564910', '#235741', '#A93B23']
+
+folder = "C:/Users/aruppel/Documents/_forcetransmission_in_cell_doublets_raw/"
+figfolder = folder + "_Figure6/"
+if not os.path.exists(figfolder):
+    os.mkdir(figfolder)
 
 mpl.rcParams['font.size'] = 8
 stressmappixelsize = 1.296 * 1e-6  # in meter
 
 # %% load data for plotting
-folder = "C:/Users/Balland/Documents/_forcetransmission_in_cell_doublets_alldata/"
-
 tissues_lefthalf_stim = pickle.load(open(folder + "analysed_data/tissues_lefthalf_stim.dat", "rb"))
 tissues_tophalf_stim = pickle.load(open(folder + "analysed_data/tissues_tophalf_stim.dat", "rb"))
 
@@ -31,17 +37,6 @@ AR1to2d_halfstim = pickle.load(open(folder + "analysed_data/AR1to2d_halfstim.dat
 AR1to1d_halfstim = pickle.load(open(folder + "analysed_data/AR1to1d_halfstim.dat", "rb"))
 AR1to1s_halfstim = pickle.load(open(folder + "analysed_data/AR1to1s_halfstim.dat", "rb"))
 AR2to1d_halfstim = pickle.load(open(folder + "analysed_data/AR2to1d_halfstim.dat", "rb"))
-
-
-# define some colors for the plots
-colors_parent = ['#026473', '#E3CC69', '#77C8A6', '#D96248']
-colors_parent_dark = ['#01353D', '#564910', '#235741', '#A93B23']
-
-figfolder = "C:/Users/Balland/Documents/_forcetransmission_in_cell_doublets_alldata/_Figure6/"
-if not os.path.exists(figfolder):
-    os.mkdir(figfolder)
-
-
 
 # %% prepare dataframe for boxplots
 
@@ -284,111 +279,6 @@ plt.text(0.21 * xmax + xmin, 1.05 * ymax, 'R = ' + str(corr))
 plt.savefig(figfolder + 'C.png', dpi=300, bbox_inches="tight")
 plt.savefig(figfolder + 'C.svg', dpi=300, bbox_inches="tight")
 plt.show()
-# # %% plot figure 5C, Stress and actin anisotropy coefficient boxplot
-#
-# # define plot parameters that are valid for the whole figure
-# # ******************************************************************************************************************************************
-# colors = [colors_parent[0], colors_parent[1], colors_parent[3]]  # defines colors
-# sns.set_palette(sns.color_palette(colors))  # sets colors
-# xticklabels = ['']  # which labels to put on x-axis
-# fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(1.2, 2.6))  # create figure instance
-# # adjust space in between plots
-# plt.subplots_adjust(wspace=0, hspace=0.4)
-# # ******************************************************************************************************************************************
-#
-#
-# # Set up plot parameters for first panel
-# #######################################################################################################
-# x = 'keys2'  # variable by which to group the data
-# y = 'AIC_baseline'  # variable that goes on the y-axis
-# ymin = 0  # minimum value on y-axis
-# ymax = 1  # maximum value on y-axis
-# yticks = np.arange(0, 1.01, 0.5)  # define where to put major ticks on y-axis
-# ylabel = None  # which label to put on y-axis
-# title = 'Mechanical \n polarization'  # title of plot
-#
-# # make plots
-# make_box_and_swarmplots(x, y, df, axes[0], ymin, ymax, yticks, xticklabels, ylabel, title, colors)
-#
-# title = 'Structural \n polarization (actin)'  # title of plot
-# y = 'actin_anisotropy_coefficient'  # variable that goes on the y-axis
-# make_box_and_swarmplots(x, y, df, axes[1], ymin, ymax, yticks, xticklabels, ylabel, title, colors)
-#
-# # # save plot to file
-# plt.savefig(figfolder + 'C.png', dpi=300, bbox_inches="tight")
-# plt.savefig(figfolder + 'C.svg', dpi=300, bbox_inches="tight")
-# plt.show()
-# # %% filter data to remove cells that have an unstable baseline
-#
-# def filter_data_main(data, threshold, title):
-#     # concatenate data on which it will be determined which cells will be filtered
-#     filterdata = np.stack(
-#         (data["TFM_data"]["relEs"][0:20, :], data["MSM_data"]["relsigma_xx"][0:20, :], data["MSM_data"]["relsigma_yy"][0:20, :],
-#          data["MSM_data"]["relsigma_xx_left"][0:20, :], data["MSM_data"]["relsigma_xx_right"][0:20, :],
-#          data["MSM_data"]["relsigma_yy_left"][0:20, :], data["MSM_data"]["relsigma_yy_right"][0:20, :]))
-#
-#     # move axis of variable to the last position for consistency
-#     filterdata = np.moveaxis(filterdata, 0, -1)
-#
-#     # maximal allowed slope for linear fit of baseline
-#     baselinefilter = create_baseline_filter(filterdata, threshold)
-#
-#     # remove cells with unstable baselines
-#     data["TFM_data"] = apply_filter(data["TFM_data"], baselinefilter)
-#     data["MSM_data"] = apply_filter(data["MSM_data"], baselinefilter)
-#
-#     new_N = np.sum(baselinefilter)
-#     print(title + ": " + str(baselinefilter.shape[0] - new_N) + " cells were filtered out")
-#
-#     return data
-#
-# # maximal allowed slope for linear fit of baseline
-# threshold = 0.1
-#
-# # tissues_full_stim = filter_data_main(tissues_full_stim, "tissues_full_stim")
-# tissues_lefthalf_stim = filter_data_main(tissues_lefthalf_stim, threshold, "tissues_lefthalf_stim")
-# tissues_tophalf_stim = filter_data_main(tissues_tophalf_stim, threshold, "tissues_tophalf_stim")
-#
-# # %% prepare dataframe again after filtering
-#
-# # initialize empty dictionaries
-# concatenated_data_lefthalf_stim = {}
-# concatenated_data_tophalf_stim = {}
-# concatenated_data = {}
-#
-# # loop over all keys
-# for key1 in tissues_lefthalf_stim:  # keys are the same for all dictionaries so I'm just taking one example here
-#     for key2 in tissues_lefthalf_stim[key1]:
-#         if tissues_lefthalf_stim[key1][key2].ndim == 1:  # only 1D data can be stored in the data frame
-#             # concatenate values from different experiments
-#             concatenated_data_lefthalf_stim[key2] = tissues_lefthalf_stim[key1][key2]
-#             concatenated_data_tophalf_stim[key2] = tissues_tophalf_stim[key1][key2]
-#
-#             concatenated_data[key2] = np.concatenate(
-#                 (concatenated_data_lefthalf_stim[key2], concatenated_data_tophalf_stim[key2]))
-#
-# key2 = 'Es_baseline'
-# # get number of elements for both condition
-# n_ls = concatenated_data_lefthalf_stim[key2].shape[0]
-# n_ts = concatenated_data_tophalf_stim[key2].shape[0]
-#
-# # create a list of keys with the same dimensions as the data
-# keysls = ['ls' for i in range(n_ls)]
-# keysts = ['ts' for i in range(n_ts)]
-#
-# keys = np.concatenate((keysls, keysts))
-#
-# # add keys to dictionary with concatenated data
-# concatenated_data['keys'] = keys
-#
-# # Creates DataFrame
-# df = pd.DataFrame(concatenated_data)
-#
-# # convert to more convenient units for plotting
-# df_plot_units = df  # all units here are in SI units
-# df_plot_units['Es_baseline'] *= 1e12  # convert to fJ
-# df_plot_units['sigma_xx_baseline'] *= 1e3  # convert to mN/m
-# df_plot_units['sigma_yy_baseline'] *= 1e3  # convert to mN/m
 
 #%% calculate stress increase ratio of right vs all
 sigma_xx_x_profile_increase_tophalf = np.nanmean(tissues_tophalf_stim["MSM_data"]["sigma_xx_x_profile_increase"], axis=1)
@@ -711,154 +601,3 @@ for ax in axes.flat:
 fig.savefig(figfolder + 'G.png', dpi=300, bbox_inches="tight")
 fig.savefig(figfolder + 'G.svg', dpi=300, bbox_inches="tight")
 plt.show()
-#  # %% plot figure 5C correlation plot of stress anisotropy and actin anisotropy
-# #
-# # # set up global plot parameters
-# # # ******************************************************************************************************************************************
-# # xticklabels = ['1to2', '1to1', '2to1']  # which labels to put on x-axis
-# # fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(2.5, 2.5))  # create figure and axes
-# # plt.subplots_adjust(wspace=0.45, hspace=0.45)  # adjust space in between plots
-# #
-# #
-# # ylabeloffset = -7
-# # xlabeloffset = 0
-# # colors = [colors_parent[0], colors_parent[1], colors_parent[3]]  # defines colors for scatterplot
-# #
-# # y = 'actin_anisotropy_coefficient'
-# # x = 'AIC_baseline'
-# # hue = 'keys'
-# # ymin = -0.5
-# # ymax = 0.5
-# # xmin = -1
-# # xmax = 1
-# # yticks = np.arange(-0.5, 0.6, 0.25)
-# # xticks = np.arange(-1, 1.1, 0.5)
-# # ylabel = "Degree of actin anisotropy"  # "'$\mathrm{\sigma_{x, MSM}}$'
-# # xlabel = "Degree of stress anisotropy"  # '$\mathrm{\sigma_{x, CM}}$'
-# #
-# # corr, p = make_correlationplotsplots(x, y, hue, df, ax, xmin, xmax, ymin, ymax, xticks, yticks, xlabel, ylabel, colors)
-# #
-# # # add line with slope 1 for visualisation
-# # # ax.plot([ymin, ymax], [0, 0], linewidth=0.5, linestyle=':', color='grey')
-# # # ax.plot([45, 45], [xmin, xmax], linewidth=0.5, linestyle=':', color='grey')
-# #
-# # plt.text(0.21 * xmax + xmin, 1.05 * ymax, 'R = ' + str(corr))
-# # # plt.text(0.52 * xmax, 1.1 * ymax, 'p = ' + '{:0.2e}'.format(p))
-# #
-# # plt.savefig(figfolder + 'C.png', dpi=300, bbox_inches="tight")
-# # plt.savefig(figfolder + 'C.svg', dpi=300, bbox_inches="tight")
-# # plt.show()
-# def circular_hist(ax, x, bins=16, density=True, offset=0, gaps=True):
-#     """
-#     Produce a circular histogram of angles on ax.
-#
-#     Parameters
-#     ----------
-#     ax : matplotlib.axes._subplots.PolarAxesSubplot
-#         axis instance created with subplot_kw=dict(projection='polar').
-#
-#     x : array
-#         Angles to plot, expected in units of radians.
-#
-#     bins : int, optional
-#         Defines the number of equal-width bins in the range. The default is 16.
-#
-#     density : bool, optional
-#         If True plot frequency proportional to area. If False plot frequency
-#         proportional to radius. The default is True.
-#
-#     offset : float, optional
-#         Sets the offset for the location of the 0 direction in units of
-#         radians. The default is 0.
-#
-#     gaps : bool, optional
-#         Whether to allow gaps between bins. When gaps = False the bins are
-#         forced to partition the entire [-pi, pi] range. The default is True.
-#
-#     Returns
-#     -------
-#     n : array or list of arrays
-#         The number of values in each bin.
-#
-#     bins : array
-#         The edges of the bins.
-#
-#     patches : `.BarContainer` or list of a single `.Polygon`
-#         Container of individual artists used to create the histogram
-#         or list of such containers if there are multiple input datasets.
-#     """
-#     # Wrap angles to [-pi, pi)
-#     x = (x+np.pi) % (2*np.pi) - np.pi
-#
-#     # Force bins to partition entire circle
-#     if not gaps:
-#         bins = np.linspace(-np.pi, np.pi, num=bins+1)
-#
-#     # Bin data and record counts
-#     n, bins = np.histogram(x, bins=bins)
-#
-#     # Compute width of each bin
-#     widths = np.diff(bins)
-#
-#     # By default plot frequency proportional to area
-#     if density:
-#         # Area to assign each bin
-#         area = n / x.size
-#         # Calculate corresponding bin radius
-#         radius = (area/np.pi) ** .5
-#     # Otherwise plot frequency proportional to radius
-#     else:
-#         radius = n
-#
-#     # Plot data on ax
-#     patches = ax.bar(bins[:-1], radius, zorder=1, align='edge', width=widths,
-#                      edgecolor='C0', fill=False, linewidth=1)
-#
-#     # Set the direction of the zero angle
-#     ax.set_theta_offset(offset)
-#
-#     # Remove ylabels for area plots (they are mostly obstructive)
-#     if density:
-#         ax.set_yticks([])
-#
-#     return n, bins, patches
-#
-# # angles0 = np.random.normal(loc=0, scale=1, size=10000)
-# # angles1 = np.random.uniform(0, 2*np.pi, size=1000)
-# force_angles2= tissues_tophalf_stim["TFM_data"]["force_angles_weighted"] #Angles des forces sommées sur toute l'image pondérés par l'intensité
-# fx_top = tissues_tophalf_stim["TFM_data"]["Tx"]* (stressmappixelsize ** 2) #vecteurs forces par pixel
-# fy_top = tissues_tophalf_stim["TFM_data"]["Ty"]* (stressmappixelsize ** 2)
-# fx_left = tissues_lefthalf_stim["TFM_data"]["Tx"]* (stressmappixelsize ** 2) #vecteurs forces par pixel
-# fy_left = tissues_lefthalf_stim["TFM_data"]["Ty"]* (stressmappixelsize ** 2)
-# fx_all = np.concatenate((fx_top,fx_left),axis=3)
-# fy_all = np.concatenate((fy_top,fy_left),axis=3)
-#
-# f_norm=np.sqrt(fy_all[:,:,0:20,:]**2 + fx_all[:,:,0:20,:]**2)
-# f_norm_mean = np.nanmean(f_norm[:,:,0:20,:], axis=2)
-# weight_factors=np.rint(f_norm_mean*(1/np.min(f_norm_mean)))
-# weight_factors_list=np.reshape(weight_factors,(120*120*107,1))
-# force_angles1=-np.abs(np.arctan(np.divide(fy_left[:,:,0:20,:], fx_left[:,:,0:20,:])))
-# # force_angles1=np.arctan2(fy_all[:,:,0:20,:], fx_all[:,:,0:20,:])
-# force_angles_mean = np.nanmean(force_angles1[:,:,0:20,:], axis=2)
-# force_angles_mean_list=np.reshape(force_angles_mean,(120*120*60,1))
-#
-# force_angle_final=[]
-# for i in range (0, len(force_angles_mean_list)):
-#     force_angle_final.extend([round(float(force_angles_mean_list[i]),8)]*int(weight_factors_list[i]))
-# force_angle_final=np.array(force_angle_final)
-#
-# actin_angles_top = tissues_tophalf_stim["shape_data"]["actin_angles"]*np.pi/180
-# actin_angles_left = tissues_lefthalf_stim["shape_data"]["actin_angles"]*np.pi/180
-# actin_angles_all = np.concatenate((actin_angles_top,actin_angles_left),axis=0)-np.pi/2
-#
-# a=np.arange(0,2*np.pi,0.1)
-# # Construct figure and axis to plot on
-# fig, axes = plt.subplots(1, 2, subplot_kw=dict(projection='polar'))
-#
-# circular_hist(axes[0], a, bins=63, density=True)
-# circular_hist(axes[1], force_angles_mean_list, bins=20, density=True)
-#
-# plt.savefig(figfolder + 'Cbis.png', dpi=300, bbox_inches="tight")
-# plt.savefig(figfolder + 'Cbis.svg', dpi=300, bbox_inches="tight")
-#
-# plt.show()
